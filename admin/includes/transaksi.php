b519633815c23c97c3606186d704d35965ee25fc
unknown <pahleviwahyu@gmail.com>
2020-09-22 11:57:59 +0700

    master

--- admin/includes/transaksi.php
+++ admin/includes/transaksi.php
@@ -1,17 +1,22 @@
 <div class="container mt-3">
-	<h2>Laporan Penjualan</h2>
+	<h2>Laporan Penjualan (<?= $mydate ?>)</h2>
 	<hr>
 	<!-- Filter Cabang -->
-	<form action="./transaksi.php" method="post">
-		<input class="form-control col-3 float-left" type="month" value="<?= $sysdate ?>" name="date">
-		<select name="id_kasir" class="form-control col-2 float-left" value="<?= $data['jenis_barang'] ?>">
-				<option value="0">All</option>
-				<?php foreach ($data_kasir as $kasir): ?>
-				<option value="<?= $kasir['id'] ?>"><?= $kasir['nama'] ?></option>
-				<?php endforeach ?>
-			</select>
-		<button type="submit" class="btn btn-success float-left">Filter</button>
-	</form>
+	<div class="dropdown">
+    	<button type="button" class="btn btn-success dropdown-toggle btn-sm float-left" data-toggle="dropdown">Filter</button>
+		<div class="dropdown-menu" style="padding: 15px; padding-bottom: 10px;">
+		<form action="./transaksi.php" method="post">
+			<input class="form-control" type="month" value="<?= $sysdate ?>" name="date">
+			<select name="id_kasir" class="form-control" value="<?= $data['jenis_barang'] ?>">
+					<option value="0">All</option>
+					<?php foreach ($data_kasir as $kasir): ?>
+					<option value="<?= $kasir['id'] ?>"><?= $kasir['nama'] ?></option>
+					<?php endforeach ?>
+				</select>
+			<button type="submit" class="btn btn-primary">Submit</button>
+		</form>
+		</div>
+	</div>
 	<a href="index.php" class="btn btn-primary btn-sm float-right">&larr; Kembali</a>
 	<div class="clearfix"></div>
 	<hr>
--- admin/transaksi.php
+++ admin/transaksi.php
@@ -10,16 +10,18 @@ if ($_SESSION['jabatan'] == 'kasir') {
 }
 $tb_kasir 		= $conn->query("SELECT id, nama FROM tb_users WHERE jabatan = 'kasir'");
 $data_kasir 	= $tb_kasir->fetch_all(MYSQLI_ASSOC);
-$sysdate  		= $conn->query("SELECT DATE_FORMAT(SYSDATE(), '%Y-%m')")->fetch_row();
-$sysdate  		= $sysdate[0];
+$date  			= $conn->query("SELECT DATE_FORMAT(SYSDATE(), '%Y-%m')")->fetch_row();
+$sysdate  		= $date[0];
 if(isset($_POST['id_kasir']) && $_POST['id_kasir']>0){
 	$sysdate = $_POST['date'];
+	$mydate = DateTime::createFromFormat('Y-m', $sysdate)->format('F-Y');
 	$nama_kasir = $data_kasir[searchForId($_POST['id_kasir'],$data_kasir,'id')]['nama'];
 	$id = $_POST['id_kasir'];
 	$where = " WHERE tb_transaksi.id_user = $id AND DATE_FORMAT(tb_transaksi.tanggal_transaksi, '%Y-%m') = '$sysdate'";
 }
 else{
 	if(isset($_POST['id_kasir']) && $_POST['id_kasir']==0){$sysdate = $_POST['date']; $_POST['id_kasir'];}
+	$mydate = DateTime::createFromFormat('Y-m', $sysdate)->format('F-Y');
 	$nama_kasir = "All";
 	$where=" WHERE DATE_FORMAT(tb_transaksi.tanggal_transaksi, '%Y-%m') = '$sysdate'";
 }
